6aecb2c2f93164e464af3b7a464f55d4f37ee0ca
Ondřej Slabý <kron258@gmail.com>
2024-12-15 20:58:12 +0100

    Move video recording uploads to Response.OnCompleted

--- backend/Controllers/EmulationController.cs
+++ backend/Controllers/EmulationController.cs
@@ -54,40 +54,62 @@ public class EmulationController : ControllerBase
         var result = await _processManager.FinishProcessAsync(id);
         
         // TODO: do something with result.SnapshotId...
-
-        var gamePackage = await _dbContext.GamePackages.Include(p => p.Version).ThenInclude(v => v.Work).FirstAsync(p => p.Id == process.PackageId);
-        foreach (var videoFile in result.VideoFiles)
-        {
-            if (videoFile.Type == RecordingType.Screen && finishRequest.KeepScreenRecording)
+        Response.OnCompleted(async () => {
+            var gamePackage = await _dbContext.GamePackages.Include(p => p.Version).ThenInclude(v => v.Work).FirstAsync(p => p.Id == process.PackageId);
+            foreach (var videoFile in result.VideoFiles)
             {
-                var filename = Path.GetFileName(videoFile.Path);
-                var dbParatext = new Models.Archive.Paratext() {
-                    Id = Guid.NewGuid(),
-                    Filename = $"rec-{process.Id}-{filename}",
-                    Name = $"Screen of session {process.Id}",
-                    Description = $"Recording of the screen for emulation session ID: {process.Id}",
-                    Downloadable = true,
-                    GamePackage = gamePackage,
-                    Version = gamePackage.Version,
-                    Work = gamePackage.Version.Work,
-                    Source = "SessionRecording",
-                    SourceUrl = null,
-                    Thumbnail = "template:video"
-                };
-                var minioArgs = new PutObjectArgs()
-                    .WithBucket(_paratextBucket)
-                    .WithObject(dbParatext.Id.ToString())
-                    .WithFileName(videoFile.Path);
-                // TODO: check for success
-                await _minioClient.PutObjectAsync(minioArgs);
-                _dbContext.Paratexts.Add(dbParatext);
-                await _dbContext.SaveChangesAsync();
+                if (videoFile.Type == RecordingType.Screen && finishRequest.KeepScreenRecording)
+                {
+                    var filename = Path.GetFileName(videoFile.Path);
+                    var dbParatext = new Models.Archive.Paratext() {
+                        Id = Guid.NewGuid(),
+                        Filename = $"rec-{process.Id}-{filename}",
+                        Name = $"Screen of session at {DateTime.UtcNow}",
+                        Description = $"Recording of the screen for emulation session ID: {process.Id}",
+                        Downloadable = true,
+                        GamePackage = gamePackage,
+                        Version = gamePackage.Version,
+                        Work = gamePackage.Version.Work,
+                        Source = "SessionRecording",
+                        SourceUrl = null,
+                        Thumbnail = "template:video"
+                    };
+                    var minioArgs = new PutObjectArgs()
+                        .WithBucket(_paratextBucket)
+                        .WithObject(dbParatext.Id.ToString())
+                        .WithFileName(videoFile.Path);
+                    // TODO: check for success
+                    await _minioClient.PutObjectAsync(minioArgs);
+                    _dbContext.Paratexts.Add(dbParatext);
+                    await _dbContext.SaveChangesAsync();
+                }
+                else if (videoFile.Type == RecordingType.Webcam && finishRequest.KeepWebcamRecording)
+                {
+                    var filename = Path.GetFileName(videoFile.Path);
+                    var dbParatext = new Models.Archive.Paratext() {
+                        Id = Guid.NewGuid(),
+                        Filename = $"rec-{process.Id}-{filename}",
+                        Name = $"Webcam of session at {DateTime.UtcNow}",
+                        Description = $"Recording of the webcam for emulation session ID: {process.Id}",
+                        Downloadable = true,
+                        GamePackage = gamePackage,
+                        Version = gamePackage.Version,
+                        Work = gamePackage.Version.Work,
+                        Source = "SessionRecording",
+                        SourceUrl = null,
+                        Thumbnail = "template:video"
+                    };
+                    var minioArgs = new PutObjectArgs()
+                        .WithBucket(_paratextBucket)
+                        .WithObject(dbParatext.Id.ToString())
+                        .WithFileName(videoFile.Path);
+                    // TODO: check for success
+                    await _minioClient.PutObjectAsync(minioArgs);
+                    _dbContext.Paratexts.Add(dbParatext);
+                    await _dbContext.SaveChangesAsync();
+                }
             }
-            else if (videoFile.Type == RecordingType.Webcam && finishRequest.KeepWebcamRecording)
-            {
-                // TODO: upload as a paratext
-            }
-        }
+        });
 
         return Ok();
     }
