24f548e069f5ed5ddfebed3cdf55218a78e9b036
Ondřej Slabý <kron258@gmail.com>
2026-02-10 16:22:56 +0100

    Add additional string constants for CA interop

--- backend/Compatibility/CollectiveAccess/Models/Enums.cs
+++ backend/Compatibility/CollectiveAccess/Models/Enums.cs
@@ -2,25 +2,27 @@ namespace asec.Compatibility.CollectiveAccess.Models;
 
 public struct BundleCodes
 {
-    public static string OccurrenceLabel = "ca_occurrences.preferred_labels";
-    public static string OccurrenceId = "ca_occurrences.occurrence_id";
-    public static string OccurrenceRelLeftId = "ca_occurrences_x_occurrences.occurrence_left_id";
+    public static readonly string OccurrenceInternalNote = "ca_occurrences.internal_notes";
+    public static readonly string OccurrenceTypeOfWork = "ca_occurrences.type_of_work";
+    public static readonly string OccurrenceLabel = "ca_occurrences.preferred_labels";
+    public static readonly string OccurrenceId = "ca_occurrences.occurrence_id";
+    public static readonly string OccurrenceRelLeftId = "ca_occurrences_x_occurrences.occurrence_left_id";
 }
 
 public struct Tables
 {
-    public static string Occurrences = "ca_occurrences";
-    public static string OccurrencesXOccurrences = "ca_occurrences_x_occurrences";
+    public static readonly string Occurrences = "ca_occurrences";
+    public static readonly string OccurrencesXOccurrences = "ca_occurrences_x_occurrences";
 }
 
 public struct Types
 {
-    public static string Work = "work";
-    public static string WorkVersion = "work_version";
+    public static readonly string Work = "work";
+    public static readonly string WorkVersion = "work_version";
 }
 
 public struct RelationTypes
 {
-    public static string WorkManifestWorkVersion = "work_manifestation_workversion";
+    public static readonly string WorkManifestWorkVersion = "work_manifestation_workversion";
 }
 
