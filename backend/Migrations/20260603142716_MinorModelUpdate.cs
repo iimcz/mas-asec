f6ead61cc2fc5d5d371e000e021adf4ac8a88433
Ondřej Slabý <kron258@gmail.com>
2026-06-09 11:30:19 +0200

    Add migrations to the updated DB model

---
+++ backend/Migrations/20260603142716_MinorModelUpdate.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace asec.Migrations
+{
+    /// <inheritdoc />
+    public partial class MinorModelUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EmulatorVersion",
+                table: "Environments",
+                newName: "Version");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Environments",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                table: "Environments",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Environments",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Environments");
+
+            migrationBuilder.DropColumn(
+                name: "Note",
+                table: "Environments");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Environments");
+
+            migrationBuilder.RenameColumn(
+                name: "Version",
+                table: "Environments",
+                newName: "EmulatorVersion");
+        }
+    }
+}
