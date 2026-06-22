075f27f758409eb422e446356d34d1bc882581cb
Ondřej Slabý <kron258@gmail.com>
2026-06-22 16:36:11 +0200

    Add DB migration to the latest model changes

---
+++ backend/Migrations/20260617095502_RemoveRedundantWorkVersionFK.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace asec.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveRedundantWorkVersionFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DigitalObjects_WorkVersions_VersionId",
+                table: "DigitalObjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DigitalObjects_VersionId",
+                table: "DigitalObjects");
+
+            migrationBuilder.DropColumn(
+                name: "VersionId",
+                table: "DigitalObjects");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "VersionId",
+                table: "DigitalObjects",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DigitalObjects_VersionId",
+                table: "DigitalObjects",
+                column: "VersionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DigitalObjects_WorkVersions_VersionId",
+                table: "DigitalObjects",
+                column: "VersionId",
+                principalTable: "WorkVersions",
+                principalColumn: "Id");
+        }
+    }
+}
--- backend/Models/Emulation/PlayableObject.cs
+++ backend/Models/Emulation/PlayableObject.cs
@@ -7,6 +7,5 @@ public class PlayableObject : DigitalObject
     public string ObjectId { get; set; }
     public DateTime CreationDate { get; set; }
     public IList<DigitalObject> IncludedDigitalObjects { get; set; }
-    public WorkVersion Version { get; set; }
     public EmulationEnvironment Environment { get; set; }
 }
