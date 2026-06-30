bca60382b3c261d1f06a7136c3c9f39805e61b82
Ondřej Slabý <kron258@gmail.com>
2026-06-30 18:50:36 +0200

    Add migration to change Date property on Paratexts to string

---
+++ backend/Migrations/20260630163924_UseStringForParatextDate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace asec.Migrations
+{
+    /// <inheritdoc />
+    public partial class UseStringForParatextDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Date",
+                table: "Paratexts",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Date",
+                table: "Paratexts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1),
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
