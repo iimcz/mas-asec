dda6aae91d7688c03a2f3c69718f1ac9431622e9
Ondřej Slabý <kron258@gmail.com>
2026-06-30 18:47:08 +0200

    Add specialized bundle value retrieval for CA bundle list extensions

--- backend/Compatibility/CollectiveAccess/Extensions.cs
+++ backend/Compatibility/CollectiveAccess/Extensions.cs
@@ -8,4 +8,13 @@ public static class IListExtensions
     {
         return bundles?.FirstOrDefault(b => b.Code == bundleCode)?.Values?.FirstOrDefault()?.Value;
     }
+
+    public static int? GetOptionalBundleIntValue(this IList<Bundle> bundles, string bundleCode)
+        => int.TryParse(bundles.GetOptionalBundleValue(bundleCode), out var result) ? result : null;
+
+    public static uint? GetOptionalBundleUintValue(this IList<Bundle> bundles, string bundleCode)
+        => uint.TryParse(bundles.GetOptionalBundleValue(bundleCode), out var result) ? result : null;
+
+    public static DateOnly? GetOptionalBundleDateValue(this IList<Bundle> bundles, string bundleCode)
+        => DateOnly.TryParse(bundles.GetOptionalBundleValue(bundleCode), out var result) ? result : null;
 }
