ba8adacd4c4f51a2461ce869edada5050a7d52a1
virusek20 <gmoder.virusek20@gmail.com>
2026-08-12 03:15:12 +0200

    Add DB migrations

---
+++ backend/Migrations/20260811220843_DigitalObject_ObjectId.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace asec.Migrations
+{
+    /// <inheritdoc />
+    public partial class DigitalObject_ObjectId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE DigitalObjects SET ObjectId = PlayableObject_ObjectId WHERE DigitalObjectType == 1;");
+
+            migrationBuilder.DropColumn(
+                name: "PlayableObject_ObjectId",
+                table: "DigitalObjects");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PlayableObject_ObjectId",
+                table: "DigitalObjects",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.Sql("UPDATE DigitalObjects SET PlayableObject_ObjectId = ObjectId WHERE DigitalObjectType == 1;");
+        }
+    }
+}
